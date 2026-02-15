e69ed53cc72698138a5a91f23d2730dfa6670fe8
omll.nanayakkara <omll.nanayakkara@gmail.com>
2026-02-15 13:29:48 +0530

    workflow updated

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": import.meta.env.BASE_URL,
-      secure: false,
-    },
-  },
   plugins: [react()],
 });
